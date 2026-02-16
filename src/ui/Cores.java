287c373bdda8b07a9c90059605b64a392d4b6f8a
Vinícius Ares <vinicius.ares12@gmail.com>
2026-02-15 22:52:48 -0300

    chore: adicionando classes utilitárias para UI

---
+++ src/ui/Cores.java
@@ -0,0 +1,11 @@
+package ui;
+
+public class Cores {
+    public static final String RESET = "\u001B[0m";
+
+    public static final String BLUE = "\u001B[34m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String LARANJA = "\u001B[38;5;208m";
+    public static final String RED = "\u001B[31m";
+}
---
+++ src/ui/Estilo.java
@@ -0,0 +1,6 @@
+package ui;
+
+public class Estilo {
+    public static final String BOLD = "\u001B[1m";
+    public static final String UNDERLINE = "\u001B[4m";
+}
